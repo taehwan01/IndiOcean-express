83c3237c3d8e829625a369915438f3e2d3f0e308
unknown <monte6198@gmail.com>
2022-09-25 23:14:51 +0900

    ADD database

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "9080",
+  database: "indiocean",
+});
+
+connection.connect();
+
+connection.query("SELECT * FROM track", function (error, results, fields) {
+  if (error) console.log(error);
+  console.log("What's in indiocean");
+  console.log(results);
+});
+
+connection.end();
 
 app.listen(8080, function () {
   console.log("Server listening on port 8080...");
