ebf26106eac7e3a121a9df9e67d1ba159d8c0430
unknown <monte6198@gmail.com>
2022-09-26 04:06:55 +0900

    ADD track api

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mysql = require("mysql");
+const { response } = require("express");
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -10,15 +11,32 @@ const connection = mysql.createConnection({
   database: "indiocean",
 });
 
-connection.connect();
+// connection.connect();
 
-connection.query("SELECT * FROM track", function (error, results, fields) {
-  if (error) console.log(error);
-  console.log("What's in indiocean");
-  console.log(results);
+app.get("/api/track", function (req, res) {
+  connection.query("SELECT * FROM track", function (error, results, fields) {
+    if (error) console.log(error);
+    console.log("PRINT track DATA: ");
+    console.log(results);
+    res.json(results);
+  });
 });
 
-connection.end();
+app.get("/api/playlist", function (req, res) {
+  connection.query("SELECT * FROM playlist", function (error, results, fields) {
+    if (error) console.log(error);
+    console.log("PRINT playlist DATA: ");
+    console.log(results);
+  });
+});
+
+app.get("/api/lists", function (req, res) {
+  connection.query("SELECT * FROM lists", function (error, results, fields) {
+    if (error) console.log(error);
+    console.log("PRINT lists DATA: ");
+    console.log(results);
+  });
+});
 
 app.listen(8080, function () {
   console.log("Server listening on port 8080...");
@@ -29,3 +47,7 @@ app.use(express.static(path.join(__dirname, "../IndiOcean-/build")));
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "../IndiOcean-/build/index.html"));
 });
+
+app.get("/api/track/add", function (req, res) {});
+
+// connection.end();
