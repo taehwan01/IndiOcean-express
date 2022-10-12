e64a13d1ac386613ee416622db3069c11764994d
Monte <monte6198@gmail.com>
2022-10-13 02:05:52 +0900

    ADD cors

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from "express";
 // import path from "path";
 import mysql from "mysql";
+import cors from "cors";
 
 import router from "./routes/index.js";
 
 const app = express();
 
+app.use(cors());
 app.use("/", router);
 
 app.listen(8080, () => {
